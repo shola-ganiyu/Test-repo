605b111a30fb412e053bd48622a404a687971c0e
shola <shola.ganiyu@yahoo.com>
2017-05-28 05:55:38 -0500

    new file

---
+++ bank.php
@@ -0,0 +1,21 @@
+<?php
+class Bank Account{
+	$balance = 0;
+
+	public function Deposit(){
+		$this->$balance
+	}
+	public function Withdraw($amount){
+		$this->$balance=$this->$balance-$amount
+		
+
+
+	}
+
+class SavingsAccount extends BankAccount{
+
+
+}
+
+}
+?>
